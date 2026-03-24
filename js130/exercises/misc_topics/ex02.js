e8eb0e4aa1dd46387fab8238e9fce0f1d7d2b299
Tim <N/A>
2026-03-24 16:03:36 +0000

    submitting js130 misc topics ex02.js

---
+++ js130/exercises/misc_topics/ex02.js
@@ -0,0 +1,16 @@
+/*
+How can we refactor the invocation of the function formatName (without changing the function definition) 
+so that we don't need to grab each element of the array fullName by index?
+*/
+
+
+function formatName(firstName, middleName, lastName) {
+  return `${lastName}, ${firstName} ${middleName[0]}.`;
+}
+
+let fullName = ['James', 'Tiberius', 'Kirk'];
+
+console.log(formatName(fullName[0], fullName[1], fullName[2]));
+// logs: Kirk, James T.
+
+console.log(formatName(...fullName));
